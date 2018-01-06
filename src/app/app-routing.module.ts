5383b53ef5310b05908bb8f736083af079a8351f
郑欣 <zhengxin@zhengxins-Mac-mini.local>
2018-01-07 00:52:37 +0800

    angularv4: children components, and corresponding usage of input() and output().

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,18 @@
 import { NgModule }     from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
+// import { LoginComponent } from './login/login.component'; // disable login
 
 const routes: Routes = [
   {
     path: '',
-    // redirectTo: 'login',// disable login
+    // redirectTo: 'login', // disable login
     redirectTo: 'todo',
     pathMatch: 'full'
   },
-  {
-    path: 'login',
-    component: LoginComponent
-  },
+  // {
+  //   path: 'login',
+  //   component: LoginComponent
+  // },
   {
     path: 'todo',
     redirectTo: 'todo/ALL'
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,24 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { CoreModule } from './core/core.module';
+// import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { TodoModule } from './todo/todo.module';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+// import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    // LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     AppRoutingModule,
-    CoreModule,
+    // CoreModule,
     TodoModule
   ],
   bootstrap: [AppComponent]
